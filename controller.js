1613bde1f2ceb25dba0ff53783b47557b2e24b6c
viery <vierydarmawan2@gmail.com>
2020-07-16 08:59:13 +0700

    terbaru

--- controller.js
+++ controller.js
@@ -48,7 +48,7 @@ exports.index = function (req, res) {
   (async () => {
     try {
       let browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
